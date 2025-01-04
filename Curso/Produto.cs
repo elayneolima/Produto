66cc7808c7e4c5c74edbc08e268c1c066f71eb99
elayneolima <elayne04lima@gmail.com>
2025-01-04 14:51:02 -0300

    Adicionando pratica de classes e metodos

---
+++ Curso/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization; 
+
+namespace Curso
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade; 
+
+        public double ValorTotalEmEstoque() 
+        {
+            return Quantidade * Preco;         
+        }
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+
+        }
+
+    }
+}
---
+++ Curso/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization; 
+using Curso; 
+
+Produto produto = new Produto();
+
+Console.WriteLine("Entre com o nome do produto");
+
+produto.Nome = Console.ReadLine(); 
+Console.WriteLine("Entre com o preço do produto");
+
+produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Entre com a quantidade do produto");
+produto.Quantidade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dados do produto: " + produto); 
+
+
+
